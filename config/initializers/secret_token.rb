23a0fa5b3975d2a92afd23e732542f9044ff0594
Anshul Jain <mosquito242@gmail.com>
2013-10-01 12:01:59 -0700

    First commit - woohoo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shnack::Application.config.secret_token = 'afdad775724ed23f7c67f7490d6a5a8c7546f9284dc829f65a294db2a42fb76f4dcc1223bb8ef411f8e9f68d5b13658e31bdf613a839f28cf4d57d5959d251f6'
