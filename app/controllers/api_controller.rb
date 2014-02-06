ac9fda6d382e61891ed880efcd5e71ba38daf17d
Anshul Jain <mosquito242@gmail.com>
2014-02-06 00:04:39 -0800

    beginning the process of security

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,22 @@
 class ApiController < ApplicationController
+	# http_basic_authenticate_with name: "admin", password: "secret"
+	before_filter :restrict_access
 	def index
 		render json: User.all
 	end
+
+	
+	# SHOULD BE PRIVATE BUT BREAKS IF I MAKE IT SO (unexpected keyword end when surrounding with private ... end)
+	def restrict_access
+		# api_key = ApiKey.find_by_access_token(params[:access_token])
+		# head :unauthorized unless api_key
+
+		authenticate_or_request_with_http_token do |token, options|
+			ApiKey.exists?(access_token: token)
+		end
+	end
+
+
+	
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131219013405) do
+ActiveRecord::Schema.define(:version => 20140206072215) do
+
+  create_table "api_keys", :force => true do |t|
+    t.string   "access_token"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
 
   create_table "items", :force => true do |t|
     t.string   "name"
