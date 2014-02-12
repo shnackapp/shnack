049d1168b880650d20c2d96ff2731d852594a3e3
Anshul Jain <mosquito242@gmail.com>
2014-02-12 10:35:36 -0800

    added user login to api

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -7,8 +7,20 @@ class ApiController < ApplicationController
 		respond_with User.all
 	end
 
+
+	# HTTP Request for when the device turns on and sends in it's device token
+	def receive_device_token
+		@device = Device.where(:token =>params[:device_token])
+		if @device
+			render :json => @device
+		else
+			render :json => {:error => "unregistered_device", :token => params[:device_token]}
+		end
+
+	end
+
+
 	def register_device_token
-		byebug
 		@vendor = Vendor.find(params[:vendor_id])
 		# unless Device.exists?(:token => params[:device_token])
 		# 	@device = @vendor.devices.create(:token => params[:device_token])
@@ -19,6 +31,26 @@ class ApiController < ApplicationController
 		@device = Device.where(:token => params[:device_token]).first_or_create 
 		render :json => @device
 	end
+
+	# Post request to log a user in.
+	# If the user is successfully logged in, it sends back the users authentication token
+	def login
+		@user = User.where(:email => params[:email]).first
+		@response = Hash.new
+
+		if @user
+			if @user.valid_password?(params[:password])
+				@response[:auth_token] =  @user.authentication_token
+			else
+				@response[:error] = "incorrect_password"
+			end
+		else
+			@response[:error] = "incorrect_email"
+		end
+
+		respond_with(@response, :location => nil)
+	end
+
 
 	private
 	# SHOULD BE PRIVATE BUT BREAKS IF I MAKE IT SO (unexpected keyword end when surrounding with private ... end)
--- config/application.rb
+++ config/application.rb
@@ -40,7 +40,7 @@ module Shnack
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :auth_token]
 
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
