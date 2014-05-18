dfce391591da1a92ae092c9e31d7d2afbd3702db
Anshul Jain <mosquito242@gmail.com>
2014-05-17 21:39:50 -0700

    changed the way i validate phone numbers

--- app/assets/javascripts/charges.js
+++ app/assets/javascripts/charges.js
@@ -4,16 +4,34 @@ $(document).ready(function() {
    			e.preventDefault();
    });
    $("#order-phone-number").mask("(000) 000-0000");
-   $("#order-phone-number").blur(function(e) {
-   		validateInputs();
-   });
+   // $("#order-phone-number").blur(function(e) {
+   // 		validateInputs();
+   // });
 
-   disableButton();
+   // disableButton();
    // $("#order-email").blur(function(e) {
    // 		validateInputs();
    // });
 	
 	
+	$("#confirm-button").on('click', function(e){
+		var phone_valid = validatePhoneNumber($("#order-phone-number").val());
+		if(!phone_valid) {
+			e.preventDefault();
+			$(".phone-error").slideDown(200);
+			$("#order-phone-number").addClass("error");
+		}
+	});
+	$(".stripe-button-el").on('click', function(e){
+		var phone_valid = validatePhoneNumber($("#order-phone-number").val());
+		if(!phone_valid) {
+			e.preventDefault();
+			$(".phone-error").slideDown(200);
+						$("#order-phone-number").addClass("error");
+
+		}
+	});
+
 	// $(".stripe-button-el").click(function(e) {
 	// 	var pathname = window.location.pathname;
 	// 	var num = $("#order-phone-number").val();
@@ -32,12 +50,13 @@ $(document).ready(function() {
 function validateInputs()
 {
 	// var email_valid = validateEmail($("#order-email").val());
-	var phone_valid = validatePhoneNumber($("#order-phone-number").val());
+	// var phone_valid = validatePhoneNumber($("#order-phone-number").val());
+	console.log("phone-valid is " + phone_valid);
 	if(phone_valid) {
 		enableButton();
 	}
 	else {
-		disableButton();
+		// disableButton();
 	}
 
 };
@@ -45,7 +64,6 @@ function validateInputs()
 function enableButton() {
 	$(".stripe-button-el").prop('disabled', false);
    $("#confirm-button").prop('disabled', false);
-
 }
 
 function disableButton() {
