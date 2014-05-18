709741e2aa08c6d6386426cda8163da803c8af48
Anshul Jain <mosquito242@gmail.com>
2014-05-17 22:07:29 -0700

    test

--- app/assets/javascripts/charges.js
+++ app/assets/javascripts/charges.js
@@ -17,8 +17,9 @@ $(document).ready(function() {
 	$("#confirm-button").on('click', function(e){
 		var phone_valid = validatePhoneNumber($("#order-phone-number").val());
 		console.log("phone is " + $("#order-phone-number").val());
-		console.log("Phone valid is" + phone_valid);
+		console.log("Phone valid is " + phone_valid);
 		if(!phone_valid) {
+			console.log("is preventing default and handling error");
 			e.preventDefault();
 			$(".phone-error").slideDown(200);
 			$("#order-phone-number").addClass("error");
