cf486c6b8dfc0dbaad12d922faa8d7f3b90c86b1
Anshul Jain <mosquito242@gmail.com>
2014-07-10 12:56:18 -0700

    made name attr accessible in usr, usrinfo

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,7 @@ class User < ActiveRecord::Base
 	# Include default devise modules. Others available are:
 	# :confirmable, :lockable, :timeoutable and :omniauthable
 	devise :database_authenticatable, :registerable,
-	 :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable
+	 :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable, :name
 
 	has_one :role
 	has_one :stadium, :through => :role
