dc604ee898ec0f64c40a027748c75bf8bd10d310
Brock Noah <brocknoah@gmail.com>
2014-07-11 18:48:43 -0700

    js

---
+++ app/assets/javascripts/restaurants.js
@@ -0,0 +1,10 @@
+# Place all the behaviors and hooks related to the matching controller here.
+# All this logic will automatically be available in application.js.
+# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+
+/*
+$(".mod_buttons").on("click", myfunction() {
+  // do something
+  alert("hello world");
+});
+*/
