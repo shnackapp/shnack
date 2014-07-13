2ba291c366d12022614a9daea469b676d01de368
Brock Noah <brocknoah@gmail.com>
2014-07-12 21:05:15 -0700

    radio button js fuck checkboxes

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -1,10 +1,9 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
-
 $(document).ready(function() {
-	initializeValues();
 
+	initializeValues();
 	$("span.plus").click(function() {
 		//increment up
 
@@ -19,6 +18,50 @@ $(document).ready(function() {
 		 $("#"+id+"_mod").slideDown(400);
 
 	});
+
+
+
+
+	$("input[type=radio]").click(function(){
+		if((this.attributes["name"].value == 'size')) {
+			var s_price = 0;
+			s_price = ($(this).data("add-price"));
+
+			semi_update_price(s_price);
+			updatePrice_mod();
+		}	else {
+			var s_price = 0;
+			s_price = ($(this).data("add-price"));
+
+			semi_update_price_2(s_price);
+			updatePrice_mod();
+		}
+	});
+
+
+$("input[type=checkbox]").click(function(){
+	var the_price = 0;
+	var price = 0;
+	$("input[type=checkbox].checked").each(function() {
+		//var $this = $(this);
+    price = $(this).data('add-price', $(this).val());
+
+    	//price = $(this).val().data("add-price");
+		
+			the_price += price;
+		});
+	alert(the_price);
+	updatePrice_num(the_price);
+});
+
+/*$("input[type=checkbox]").click(function(){
+			var s_price = 0;
+			s_price = ($(this).data("add-price"));
+
+			//semi_update_price_checkbox(s_price);
+			updatePrice_num(s_price);
+
+});*/
 
 $(document).ready(function() {
   var hidden = true;
@@ -59,6 +102,7 @@ $(document).ready(function() {
 });
 
 function initializeValues() {
+
 	//Initialize all the values of the form.
 	$(".num").each(function() {
 		// $(".num:eq(1)").parent().children().last().val()
@@ -70,6 +114,41 @@ function initializeValues() {
 
 	$(".all_the_mods").hide();
 
+
+};
+var last_click_1 = 0;
+function semi_update_price_2(the_price) {
+	temppp = temppp + the_price - last_click_1;
+	last_click_1 = the_price;
+
+};
+
+var temppp = 0;
+var last_click = 0;
+function semi_update_price(the_price) {
+	temppp = temppp + the_price - last_click;
+	last_click = the_price;
+};
+
+function updatePrice_num(num) {
+	var subtotal = 0;
+
+	$(".menu_item").each(function() {
+		var id = $(this).data("id");
+		var qty = $("#" + id+ "_num").html();
+		var price = $("#" + id + "_price").data("price");
+		price+=num;
+		var cost = qty*price;
+		subtotal += cost;
+	});
+	subtotal /=100;
+	$("td.subtotal").html(toUSD(subtotal));
+	$("td.subtotal").data("price", subtotal);
+
+	var total = $(".fee").data("price")/100 + subtotal;
+
+	$("td.total").html(toUSD(total));
+	$("td.total").data("total", total);
 
 };
 
@@ -93,6 +172,30 @@ function updatePrice() {
 	$("td.total").data("total", total);
 
 };
+
+function updatePrice_mod() {
+	var subtotal = 0;
+
+	$(".menu_item").each(function() {
+		var id = $(this).data("id");
+		var qty = $("#" + id+ "_num").html();
+		var price = $("#" + id + "_price").data("price");
+		price= price + temppp;
+		var cost = qty*price;
+		subtotal += cost;
+	});
+	subtotal /=100;
+	$("td.subtotal").html(toUSD(subtotal));
+	$("td.subtotal").data("price", subtotal);
+
+	var total = $(".fee").data("price")/100 + subtotal;
+
+	$("td.total").html(toUSD(total));
+	$("td.total").data("total", total);
+
+};
+
+
 
 function toUSD(number) {
     var number = number.toString(), 
