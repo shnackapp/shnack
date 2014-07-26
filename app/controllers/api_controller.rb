b3172841f3f966341dfd6c734cd5826f50f7319d
Spencer Neste <spencerneste@yahoo.com>
2014-07-26 10:08:18 -0700

    stripe error handling from their API reference

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -188,21 +188,40 @@ class ApiController < ApplicationController
 
 		# Create the charge on Stripe's servers - this will charge the user's card
 		begin
-		@customer = Stripe::Customer.create(
-		    :email => params[:stripeEmail],
-		    :card  => params[:stripeToken],
-		    :name => params[:stripeCardholder]
-		  )
-		rescue Stripe::CardError => e
-		# The card has been declined
-		respond_with :json => { :error => "stripe_error"}
-		end
 
-		respond_with(@customer, :location => nil)
+@customer = Stripe::Customer.create(
+	    :email => params[:stripeEmail],
+	    :card  => params[:stripeToken],
+	    :name => params[:stripeCardholder]
+	  )
 
+# Use Stripe's bindings...
+rescue Stripe::CardError => e
+# Since it's a decline, Stripe::CardError will be caught
+	body = e.json_body
+	err  = body[:error]
+	puts "Status is: #{e.http_status}"
+	puts "Type is: #{err[:type]}"
+	puts "Code is: #{err[:code]}"
+	# param is '' in this case
+	puts "Param is: #{err[:param]}"
+	puts "Message is: #{err[:message]}"
+rescue Stripe::InvalidRequestError => e
+# Invalid parameters were supplied to Stripe's API
+rescue Stripe::AuthenticationError => e
+# Authentication with Stripe's API failed
+# (maybe you changed API keys recently)
+rescue Stripe::APIConnectionError => e
+# Network communication with Stripe failed
+rescue Stripe::StripeError => e
+# Display a very generic error to the user, and maybe send
+# yourself an email
+rescue => e
+# Something else happened, completely unrelated to Stripe
+end
+respond_with(@customer, :location => nil)
 
-
-	end
+end
 
 
 	private
