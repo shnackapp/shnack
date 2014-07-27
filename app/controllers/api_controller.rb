9da4ff91c2f31a8f28d32dcdcf45c8b5f1233f86
Spencer Neste <spencerneste@yahoo.com>
2014-07-26 23:55:38 -0700

    fixed another respond_with error

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -9,7 +9,7 @@ class ApiController < ApplicationController
     @user = User.create(:name => params[:name],:email => params[:email],:number => params[:phone],:password => params[:password],:customer_id =>params[:customer_id])
 
    	#respond_with  {:auth_token => @user.authentication_token}
-   	respond_with  {auth_token:@user.authentication_token}
+   	respond_with  (auth_token:@user.authentication_token)
 
 
 
