29dd485ad3d22d7af5cbd86644f0f6df43f10757
Spencer Neste <spencerneste@yahoo.com>
2014-07-26 18:40:31 -0700

    nil fix on response

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -193,34 +193,34 @@ class ApiController < ApplicationController
 	    :card  => params[:stripeToken]
 	    )
 
-# Use Stripe's bindings...
-rescue Stripe::CardError => e
-# Since it's a decline, Stripe::CardError will be caught
-	body = e.json_body
-	err  = body[:error]
-	puts "Status is: #{e.http_status}"
-	puts "Type is: #{err[:type]}"
-	puts "Code is: #{err[:code]}"
-	# param is '' in this case
-	puts "Param is: #{err[:param]}"
-	puts "Message is: #{err[:message]}"
-rescue Stripe::InvalidRequestError => e
-# Invalid parameters were supplied to Stripe's API
-	body = e.json_body
-	err  = body[:error]
-	puts "Status is: #{e.http_status}"
+# # Use Stripe's bindings...
+# rescue Stripe::CardError => e
+# # Since it's a decline, Stripe::CardError will be caught
+# 	body = e.json_body
+# 	err  = body[:error]
+# 	puts "Status is: #{e.http_status}"
+# 	puts "Type is: #{err[:type]}"
+# 	puts "Code is: #{err[:code]}"
+# 	# param is '' in this case
+# 	puts "Param is: #{err[:param]}"
+# 	puts "Message is: #{err[:message]}"
+# rescue Stripe::InvalidRequestError => e
+# # Invalid parameters were supplied to Stripe's API
+# 	body = e.json_body
+# 	err  = body[:error]
+# 	puts "Status is: #{e.http_status}"
 
-rescue Stripe::AuthenticationError => e
-# Authentication with Stripe's API failed
-# (maybe you changed API keys recently)
-rescue Stripe::APIConnectionError => e
-# Network communication with Stripe failed
-rescue Stripe::StripeError => e
-# Display a very generic error to the user, and maybe send
-# yourself an email
-rescue => e
-# Something else happened, completely unrelated to Stripe
-end
+# rescue Stripe::AuthenticationError => e
+# # Authentication with Stripe's API failed
+# # (maybe you changed API keys recently)
+# rescue Stripe::APIConnectionError => e
+# # Network communication with Stripe failed
+# rescue Stripe::StripeError => e
+# # Display a very generic error to the user, and maybe send
+# # yourself an email
+# rescue => e
+# # Something else happened, completely unrelated to Stripe
+# end
 
 render :json => @customer
 
