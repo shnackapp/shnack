276fe543d3bb115ceed12c853e60f8b5cd7beb6b
Anshul Jain <mosquito242@gmail.com>
2014-08-06 01:33:02 -0700

    trying this to fix error

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,25 +2,27 @@
 #
 # Table name: orders
 #
-#  id            :integer          not null, primary key
-#  user_id       :integer
-#  charge_id     :string(255)
-#  subtotal      :integer
-#  vendor_id     :integer
-#  created_at    :datetime         not null
-#  updated_at    :datetime         not null
-#  details       :string(255)
-#  paid          :boolean          default(FALSE)
-#  restaurant_id :integer
-#  total         :integer
-#  slug          :string(255)
-#  slug_id       :string(255)
-#  order_number  :integer
-#  user_info_id  :integer
-#  shnack_cut    :integer          default(0)
-#  location_cut  :integer
-#  withdrawn     :boolean          default(FALSE)
-#  transfer_id   :integer
+#  id              :integer          not null, primary key
+#  user_id         :integer
+#  charge_id       :string(255)
+#  subtotal        :integer
+#  vendor_id       :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  details         :string(255)
+#  paid            :boolean          default(FALSE)
+#  restaurant_id   :integer
+#  total           :integer
+#  slug            :string(255)
+#  slug_id         :string(255)
+#  order_number    :integer
+#  user_info_id    :integer
+#  shnack_cut      :integer          default(0)
+#  location_cut    :integer
+#  withdrawn       :boolean          default(FALSE)
+#  transfer_id     :integer
+#  credit_was_used :boolean          default(FALSE)
+#  credit_used     :integer          default(0)
 #
 
 class Order < ActiveRecord::Base
