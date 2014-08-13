a5dd160b90cdf7503e6f4d67981dab129e93c506
Anshul Jain <mosquito242@gmail.com>
2014-08-13 13:36:52 -0700

    quick fix

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -192,7 +192,7 @@ class ChargesController < ApplicationController
 	
 	rescue Stripe::CardError => e
 	  flash[:error] = e.message
-	  redirect_to charges_path
+	  redirect_to new_order_charge_path(@order)
 	end
 
 
