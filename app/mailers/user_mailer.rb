14ad6f49ece66e9157d2efb9759c838baf526925
Brock Noah <brocknoah@gmail.com>
2014-08-24 15:39:04 -0700

    user mailer and view

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,4 +6,11 @@ class UserMailer < ActionMailer::Base
   	@url = 'https://www.shnackapp.com/users/sign_in'
   	mail(to: @user.email, subject: "Welcome to Shnack")
   end
+
+  def announcement_email(user)
+    @user = user
+    @bb_url = 'https://shnackapp.com/orders/new?restaurant_id=2'
+    mail(to: @user.email, subject: "Shnack is now coming to Buddha Bowls!!")
+  end
+
 end
